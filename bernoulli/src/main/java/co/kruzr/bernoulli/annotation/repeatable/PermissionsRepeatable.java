78f7761e7ef869c6c6d0e522b33febdcdd7dfad6
Yuganka Sharan <yugankasharan@gmail.com>
2021-04-19 16:38:02 +0530

    Successful evaluation of RequiresPermission and RequiresSetting on same method.

---
+++ bernoulli/src/main/java/co/kruzr/bernoulli/annotation/repeatable/PermissionsRepeatable.java
@@ -0,0 +1,22 @@
+package co.kruzr.bernoulli.annotation.repeatable;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import co.kruzr.bernoulli.annotation.RequiresPermission;
+
+/**
+ * Since a given class/method may require multiple permissions, we need to set a meta-annotation @Repeatable to
+ * RequiresPermission. That requires an intermediary class to be implemented.
+ *
+ * This is that class.
+ */
+@Inherited
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PermissionsRepeatable {
+    RequiresPermission[] value();
+}
